db3e24fe687816f43a0916c8a9a9f92f852ec3a1
Lucidreline <casta.ma502@gmail.com>
2021-04-24 16:37:16 -0700

    Python can return blogs from database as json.

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from datetime import datetime
 
 app = Flask(__name__)
+ma = Marshmallow(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blogs.db'
 # to stop it from giving us warning
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -22,3 +24,24 @@ class Blog(db.Model):
     # a toString
     def __repr__(self):
         return '<Blog %r>' % self.title
+
+
+class BlogSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "body", "date_created")
+
+
+@app.route('/blogs')
+def show_blogs():
+    blogsToReturn = Blog.query.all()
+    blogs_schema = BlogSchema(many=True)
+    output = {"blogs": blogs_schema.dump(blogsToReturn)}
+    return jsonify(output)
+
+
+@app.route('/blog/<int:blog_id>')
+def show_blog(blog_id):
+    blogToReturn = Blog.query.get(blog_id)
+    blog_schema = BlogSchema()
+    output = {"blog": blog_schema.dump(blogToReturn)}
+    return jsonify(output)
